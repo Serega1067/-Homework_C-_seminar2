c204196064b9d7e2f2f0a130b8bbb9da4ba8450f
Serega S <serega1067@yandex.ru>
2022-02-28 22:11:27 +0700

    Task1 done

---
+++ C#_seminar2_task1/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Задача 10: Напишите программу, которая принимает на вход 
+трёхзначное число и на выходе показывает вторую цифру этого числа.
+*/
+
+void foo(int arg)
+{
+    if (99 >= arg || arg >= 1000)
+    {
+        Console.WriteLine("Вы ввели число неверного " +
+                          "размера\nВведите трёхзначное число: ");
+        foo(Convert.ToInt32(Console.ReadLine()));
+    }
+    else
+    {
+        Console.WriteLine($"Вторая цифра числа: {arg / 10 % 10}");
+    }
+}
+
+Console.WriteLine("Введите трёхзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+foo(num);
