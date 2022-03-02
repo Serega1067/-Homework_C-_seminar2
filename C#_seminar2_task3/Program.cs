0f0730e8e447f7f1755fcd8caf9bc403f05dc6ed
Serega S <serega1067@yandex.ru>
2022-03-02 22:07:38 +0700

    Task3 done

---
+++ C#_seminar2_task3/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Задача 15: Напишите программу, которая принимает на вход цифру, 
+обозначающую день недели, и проверяет, является ли этот день выходным.
+*/
+
+void numberDayWeek(int arg)
+{
+    if (arg == 6 || arg == 7)
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        if (arg < 6)
+        {
+            Console.WriteLine("Нет");
+        }
+        else
+        {
+            Console.WriteLine("Вы ввели неверную цифру" +
+                              "\nВведите номер дня недели от 1 до 7 " +
+                              "чтобы узнать является ли он выходным:");
+            arg = Convert.ToInt32(Console.ReadLine());
+            numberDayWeek(arg);
+        }
+    }
+}
+Console.WriteLine("Введите номер дня недели от 1 до 7 " +
+                  "чтобы узнать является ли он выходным:");
+int num = Convert.ToInt32(Console.ReadLine());
+numberDayWeek(num);
